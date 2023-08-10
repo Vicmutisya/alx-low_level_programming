d243d037f1fc5c2e338d50c5c4f57f2fc8d39b9d
Vicmutisya <vicmutisya17@gmail.com>
2023-08-10 10:38:20 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - C program that prints followed by a new line
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
