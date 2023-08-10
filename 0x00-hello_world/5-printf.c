305af996c5b50b93a388116aacf36ac290833790
Vicmutisya <vicmutisya17@gmail.com>
2023-08-10 10:48:45 -0700

    Printing using function printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print function printf followed by a new line
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
