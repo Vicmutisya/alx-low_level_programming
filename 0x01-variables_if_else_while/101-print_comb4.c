fc4424cfde203e398e901b5505616a8c4919309d
Vicmutisya <vicmutisya17@gmail.com>
2023-08-11 13:13:52 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
 
+/**
+ * main - prints all possible different combinations in three digits
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	int j, k, l;
+
+	for (j = 48 ; j < 58 ; j++)
+	{
+		for (k = 49 ; k < 58 ; k++)
+		{
+			for (l = 50 ; l < 58 ; l++)
+			{
+				if (l > k && k > j)
+				{
+					putchar(j);
+					putchar(k);
+					putchar(l);
+					if (j != 55 || k != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
