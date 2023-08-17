3893e8c29f9cd77e8815f7d4e7fd3a10937e02b1
Vicmutisya <vicmutisya17@gmail.com>
2023-08-17 10:59:10 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,22 +16,14 @@ int main(void)
 
 	for (l = 1 ; l <= 100 ; l++)
 	{
-		if (l % 3 == 0 && l % 5 != 0)
-		{
-			printf("Fizz");
-		} else if (l % 5 == 0 && l % 3 != 0)
-		{
-			printf("Buzz");
-		} else if (l % 3 == 0 && l % 5 == 0)
-		{
-			printf("FizzBuzz");
-		} else if (l == 1)
-		{
+		if ((l % 3 == 0) && (l % 5 == 0))
+			printf("%s", "Buzz");
+		else if (l % 5 == 0)
+			printf("%s", "Buzz");
+		else
 			printf("%d", l);
-		} else
-		{
-			printf("%d", l);
-		}
+		if (l != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
