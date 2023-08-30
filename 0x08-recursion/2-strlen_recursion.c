d38a184c4afccb0dd3ba331f8f7c2ccf30a01b65
Vicmutisya <vicmutisya17@gmail.com>
2023-08-30 06:23:52 -0700

    prints string in reverse

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _stren_recursion - returns the length of a string
+ * @s: sets the string to be measured
+ *
+ * Return: the length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int longit = 0;
+
+	if (*s)
+	{
+		longit++;
+		longit += _strlen_recursion(s + 1);
+	}
+	return (longit);
+
+}
